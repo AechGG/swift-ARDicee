b9e531a5043c6e70e00bffeeba4dc3a108a9bf5e
AechGG <hazg@me.com>
2020-09-07 20:20:19 +0100

    feat: roll all dice, on refresh button and on shake

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     
+    var diceArray = [SCNNode]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -82,17 +84,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         z: hitResult.worldTransform.columns.3.z
                     );
                     
+                    diceArray.append(diceNode);
+                    
                     sceneView.scene.rootNode.addChildNode(diceNode);
                     
-                    // Roll dice
-                    let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi/2);
-                    let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi/2);
-                    
-                    diceNode.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5));
+                    roll(diceNode);
                 }
             }
         }
         
+    }
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice);
+            }
+        }
+    }
+    
+    func roll(_ dice: SCNNode) {
+        // Roll dice
+        let randomX = Float((arc4random_uniform(4) + 1)) * (Float.pi/2);
+        let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi/2);
+        
+        dice.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5));
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll();
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll();
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
