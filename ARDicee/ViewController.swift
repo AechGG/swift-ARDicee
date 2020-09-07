825b5f973f5b3ad7583520dc2f679daa8c661357
AechGG <hazg@me.com>
2020-09-07 20:30:36 +0100

    feat: refactoring

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -17,31 +17,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     var diceArray = [SCNNode]();
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.sceneView.debugOptions = [.showFeaturePoints];
+        super.viewDidLoad();
         
         // Set the view's delegate
         sceneView.delegate = self
-        
-        //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01);
-        
-        //        let sphere = SCNSphere(radius: 0.2);
-        //
-        //        let material = SCNMaterial();
-        //
-        //        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg");
-        //
-        //        sphere.materials = [material];
-        //
-        //        let node = SCNNode();
-        //
-        //        node.position = SCNVector3(0, 0.1, -0.5);
-        //
-        //        node.geometry = sphere;
-        //
-        //        sceneView.scene.rootNode.addChildNode(node);
-        //
         sceneView.autoenablesDefaultLighting = true;
     }
     
@@ -65,7 +44,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    // MARK: - ARSCNViewDelegate
+    // MARK: - Dice Rendering Methods
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
@@ -74,32 +53,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent);
             
             if let hitResult = results.first {
-                // Create a new scene
-                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!;
-                
-                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-                    diceNode.position = SCNVector3(
-                        x: hitResult.worldTransform.columns.3.x,
-                        y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
-                        z: hitResult.worldTransform.columns.3.z
-                    );
-                    
-                    diceArray.append(diceNode);
-                    
-                    sceneView.scene.rootNode.addChildNode(diceNode);
-                    
-                    roll(diceNode);
-                }
+                addDice(atLocation: hitResult);
             }
         }
         
     }
     
-    func rollAll() {
-        if !diceArray.isEmpty {
-            for dice in diceArray {
-                roll(dice);
-            }
+    func addDice(atLocation location: ARHitTestResult) {
+        // Create a new scene
+        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!;
+        
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+            diceNode.position = SCNVector3(
+                x: location.worldTransform.columns.3.x,
+                y: location.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                z: location.worldTransform.columns.3.z
+            );
+            
+            diceArray.append(diceNode);
+            
+            sceneView.scene.rootNode.addChildNode(diceNode);
+            
+            roll(diceNode);
         }
     }
     
@@ -109,6 +84,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let randomZ = Float((arc4random_uniform(4) + 1)) * (Float.pi/2);
         
         dice.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5));
+    }
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice);
+            }
+        }
     }
     
     @IBAction func rollAgain(_ sender: UIBarButtonItem) {
@@ -129,28 +112,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         rollAll();
     }
     
+    
+    // MARK: - ARSCNViewDelegate
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor is ARPlaneAnchor {
-            let planeAnchor = anchor as! ARPlaneAnchor;
-            
-            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z));
-            
-            let planeNode = SCNNode();
-            
-            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z);
-            
-            // Make the plane horizontal
-            planeNode.transform = SCNMatrix4MakeRotation(0.5 * -Float.pi, 1, 0, 0);
-            
-            let gridMaterial = SCNMaterial();
-            
-            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png");
-            
-            plane.materials = [gridMaterial];
-            
-            planeNode.geometry = plane;
-            
-            node.addChildNode(planeNode);
+        if let planeAnchor = anchor as? ARPlaneAnchor {
+            node.addChildNode(createPlane(with: planeAnchor));
         }
+    }
+    
+    // MARK: - Plane Rendering
+    
+    func createPlane(with planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z));
+        
+        let planeNode = SCNNode();
+        
+        planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z);
+        
+        // Make the plane horizontal
+        planeNode.transform = SCNMatrix4MakeRotation(0.5 * -Float.pi, 1, 0, 0);
+        
+        let gridMaterial = SCNMaterial();
+        
+        gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png");
+        
+        plane.materials = [gridMaterial];
+        
+        planeNode.geometry = plane;
+        
+        return planeNode;
     }
 }
